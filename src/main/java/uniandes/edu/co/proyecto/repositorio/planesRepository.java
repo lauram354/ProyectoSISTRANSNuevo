f6721102efc8489822b096c37249529745dd81f2
lauram354 <l.murciac@uniandes.edu.co>
2023-11-04 16:50:04 -0500

    RF6P1

---
+++ src/main/java/uniandes/edu/co/proyecto/repositorio/planesRepository.java
@@ -0,0 +1,34 @@
+package uniandes.edu.co.proyecto.repositorio;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import uniandes.edu.co.proyecto.modelo.Planes;
+
+public interface planesRepository extends JpaRepository<Planes, Integer>{
+
+    //RF6
+    @Query(value = "SELECT * FROM planes", nativeQuery = true)
+    Collection<Planes> darPlanes();
+
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO Planes( idTipoPlan,tipo_plan,descuento,Hoteles_nombre) VALUES(PLANES_IDTIPOPLAN_SEQ.nextval, tipo_plan = :tipoPlan, descuento = :descuento, Hoteles_nombre = :hotel);" , nativeQuery = true)
+    void insertarPlanes(@Param("tipoPlan") String tipoPlan, @Param("descuento") Float descuento, @Param("hotel") String hotel);
+
+    @Modifying
+    @Transactional
+    @Query(value = "update planes set tipo_plan = :tipoPlan, descuento = :descuento WHERE idtipoplan = :idplan", nativeQuery = true)
+    void actualizarPlan(@Param("tipoPlan") String tipoPlan, @Param("descuento") Integer descuento, @Param("idplan") Integer idplan);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM planes WHERE idtipoplan = :idplan", nativeQuery = true)
+    void eliminarTipoUsuario(@Param("idplan") Integer idplan);
+} 
+
