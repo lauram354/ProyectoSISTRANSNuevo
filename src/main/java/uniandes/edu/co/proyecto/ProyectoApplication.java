738c3e88d8dfbf028314968571ccb666dee56608
lauram354 <l.murciac@uniandes.edu.co>
2023-11-04 23:49:25 -0500

    RF9 y RF11

--- src/main/java/uniandes/edu/co/proyecto/ProyectoApplication.java
+++ src/main/java/uniandes/edu/co/proyecto/ProyectoApplication.java
@@ -11,15 +11,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import uniandes.edu.co.proyecto.modelo.Reservas;
 import uniandes.edu.co.proyecto.modelo.Servicios;
 import uniandes.edu.co.proyecto.modelo.Usuarios;
-import uniandes.edu.co.proyecto.modelo.reservaServ;
-import uniandes.edu.co.proyecto.repositorio.reservasServRepository;
+import uniandes.edu.co.proyecto.modelo.usuReservas;
+import uniandes.edu.co.proyecto.repositorio.usuReservasRepository;
+
 
 
 @SpringBootApplication
 public class ProyectoApplication implements CommandLineRunner{
 
 	@Autowired
-	private reservasServRepository serv;
+	private usuReservasRepository serv;
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoApplication.class, args);
 	}
@@ -27,9 +28,9 @@ public class ProyectoApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		Collection<reservaServ> servis = serv.darReservasServ();
-		for (reservaServ s : servis){
-			System.out.println(s.getIdreserva());
+		Collection<usuReservas> servis = serv.darUsuReservas();
+		for (usuReservas s : servis){
+			System.out.println(s.getCheckin());
 		}
 		
 	}
